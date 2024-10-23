e6eda03683b6e37c8167d75e1d566a6fb1add957
captainboogey <anik35-700@diu.edu.bd>
2024-10-23 08:34:06 +0600

    first commit

---
+++ script/genius.js
@@ -0,0 +1,41 @@
+function triangleBtn() {
+  const bBox = getParameter("b-box");
+  const hBox = getParameter("h-box");
+  const equation = 0.5 * bBox * hBox;
+
+  setParameter("your-answer", equation);
+  console.log(equation);
+}
+function rectangleBtn(){
+    const wBox = getParameter('w-box');
+    const iBox = getParameter('i-box');
+    const equation= wBox*iBox;
+    setParameter('rectangle-answer',equation)
+
+
+}
+function parallelogramBtn(){
+    const b2Box = getParameter('b2-box');
+    const h2Box = getParameter('h2-box');
+    const equation= b2Box*h2Box;
+    setParameter('parallelogram-answer',equation);
+}
+function rhombusBtn(){
+    const d1Box = getParameter('d1-box');
+    const d2Box = getParameter('d2-box');
+    const equation= 0.5*d1Box*d2Box;
+    setParameter('rhombus-answer',equation);
+    
+}
+
+function getParameter(writtenElement) {
+  const element = document.getElementById(writtenElement);
+  const elementvalue = element.value;
+  const elementInNumber = parseFloat(elementvalue);
+  return elementInNumber;
+}
+function setParameter(writtenParameter, equation) {
+  const element = document.getElementById(writtenParameter);
+  element.innerText = equation;
+}
+
